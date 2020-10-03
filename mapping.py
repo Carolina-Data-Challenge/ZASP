bc2132a1126c52d099c7917a1c352c6ce89ea687
Amit Parikh <amitparikh41@gmail.com>
2020-10-03 19:34:22 -0400

    mapping file

---
+++ mapping.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import plotly.express as px
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+tx = pd.read_csv("unemployment_rate_2019.csv", encoding = "ISO-8859-1")
+dtype={"fips":str}
+    
+fig = px.choropleth_mapbox(tx, geojson=counties, locations='FIPS', color='Unemployment_rate_2019',
+                           color_continuous_scale="Viridis",
+                           mapbox_style="carto-positron",
+                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           labels={'unemp':'unemployment rate'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
