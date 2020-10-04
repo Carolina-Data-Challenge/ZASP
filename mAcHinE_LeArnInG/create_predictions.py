1f106282f2f615e54d006875fecf84a389555bdb
Amit Parikh <amitparikh41@gmail.com>
2020-10-04 10:17:11 -0400

    took out saving code

--- mAcHinE_LeArnInG/create_predictions.py
+++ mAcHinE_LeArnInG/create_predictions.py
@@ -6,15 +6,4 @@ X, y, combined_data, county_codes = MLP_regressor_model.fetch_data()
 model = MLP_regressor_model.train_and_test(X, y)
 predictions = MLP_regressor_model.predict(combined_data, model)
 
-#print(predictions)
 
-## Save predictions 
-
-predictions_df = pd.DataFrame(columns=['county_code','zasp_index','Year'])
-for i in range(len(predictions)):
-    for j in range(0,3):
-        predictions_df.append({'county_code':county_codes[i],'zasp_index':predictions[i][j],'Year': (2020+j)},ignore_index=True)
-        
-predictions_df['zasp_index'] = predictions_df['zasp_index'].transform(lambda x: min(x, 100))
-predictions_df.to_csv("predictions.csv",index = False)
-# TODO Predictions might produce values higher than 100...
