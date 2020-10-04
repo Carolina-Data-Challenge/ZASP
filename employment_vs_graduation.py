732f366371a36b65ad4c5a1216831c88ee4c1699
ZaRivers <zuntue@gmail.com>
2020-10-04 04:26:49 -0400

    formatting

--- employment_vs_graduation.py
+++ employment_vs_graduation.py
@@ -1,28 +1,26 @@
-import pandas as pd 
-import seaborn as sns
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import seaborn as sns
 
 ## Get in two data frames
 unemployment_2019_df = pd.read_csv("unemployment_rate_2019.csv")
 graduation_rate_df = pd.read_csv("hs_grad_rate.csv")
-graduation_rate_df = graduation_rate_df[['FIPS','hs_grad_rate']]
+graduation_rate_df = graduation_rate_df[['FIPS', 'hs_grad_rate']]
 
 ## Combine data frames
-combined_df = pd.merge(unemployment_2019_df,graduation_rate_df,on='FIPS')
-combined_df = combined_df[['FIPS','Unemployment_rate_2019','hs_grad_rate']]
+combined_df = pd.merge(unemployment_2019_df, graduation_rate_df, on='FIPS')
+combined_df = combined_df[['FIPS', 'Unemployment_rate_2019', 'hs_grad_rate']]
 # Multiply percent by 100
-combined_df['hs_grad_rate'] = combined_df['hs_grad_rate'].apply(lambda x: x*100)
+combined_df['hs_grad_rate'] = combined_df['hs_grad_rate'].apply(lambda x: x * 100)
 # Get employment rate instead of unemployment
-combined_df['Unemployment_rate_2019'] = combined_df['Unemployment_rate_2019'].apply(lambda x:100-x)
-#rename
-combined_df = combined_df.rename(columns={"Unemployment_rate_2019":"Employment Rate"})
+combined_df['Unemployment_rate_2019'] = combined_df['Unemployment_rate_2019'].apply(lambda x: 100 - x)
+# rename
+combined_df = combined_df.rename(columns={"Unemployment_rate_2019": "Employment Rate"})
 
 ## Create plot
-sns.regplot(x='Employment Rate',y="hs_grad_rate", data = combined_df)
+sns.regplot(x='Employment Rate', y="hs_grad_rate", data=combined_df)
 # Titles
 plt.xlabel("Employment Rate")
 plt.ylabel("HS Graduation Rate")
 plt.title("Employment Rate vs HS Graduation Rate in US counties (2019)")
 plt.show()
-
